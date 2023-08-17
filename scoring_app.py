a0f7f5b010b9b5952dbfa4cb9a314e0b20b22324
dorksterdave <67047413+dorksterdave@users.noreply.github.com>
2023-08-17 19:32:50 +0400

    Update scoring_app.py

--- scoring_app.py
+++ scoring_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly as pt
+# import plotly as pt
 import plotly.express as px
 import seaborn as ss
 from sklearn.preprocessing import MinMaxScaler
